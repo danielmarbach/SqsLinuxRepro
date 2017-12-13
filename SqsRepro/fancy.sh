be83544ac7cbfc4cab188be34268e15b76495467
danielmarbach <daniel.marbach@openplace.net>
2017-12-13 11:48:45 +0100

    Build first then execute

--- SqsRepro/fancy.sh
+++ SqsRepro/fancy.sh
@@ -1,6 +1,7 @@
 #
 #!/bin/bash
 echo "Bash version ${BASH_VERSION}..."
+dotnet build SqsRepro.csproj -c Release
 for i in {1..5} 
 do
    dotnet run SqsRepro.csproj -c Release --no-rebuild --no-restore
